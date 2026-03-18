530581d94bfd9f19e63a55236f57c876d69f7472
vinay kumar <vinaykumarps321@gmail.com>
2026-03-18 11:37:04 +0530

    add

--- main.go
+++ main.go
@@ -2,6 +2,29 @@ package main
 
 import "github.com/gin-gonic/gin"
 
+type Note struct {
+	ID    int    `json:"id"`
+	Title string `json"title"`
+	Body  string `json"body"`
+}
+
+var notes = make(map[int]Note)
+var currentID = 1
+
+func createNote(c *gin.Context) {
+	var newNote Note
+
+	if err := c.ShouldBindJSON(&newNote); err != nil {
+		c.JSON(400, gin.H{"error": err.Error()})
+		return
+	}
+	newNote.ID = currentID
+	notes[currentID] = newNote
+	currentID++
+
+	c.JSON(201, newNote)
+}
+
 func main() {
 	r := gin.Default()
 
@@ -10,5 +33,7 @@ func main() {
 			"message": "api up",
 		})
 	})
+
+	r.POST("/notes", createNote)
 	r.Run(":8080")
 }
